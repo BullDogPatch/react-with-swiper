9ebb07d18d087351d6d2dd84097d9e0f87b24cfc
Craig Clayton <craig__clayton@outlook.com>
2020-08-08 00:55:20 +0100

    .

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import SwiperCore from 'swiper';
+import 'swiper/swiper-bundle.css';
+
+import './styles.css';
+
+function App() {
+  const slides = [
+    'https://images.unsplash.com/photo-1449034446853-66c86144b0ad?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2100&q=80',
+    'https://images.unsplash.com/photo-1470341223622-1019832be824?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2288&q=80',
+    'https://images.unsplash.com/photo-1448630360428-65456885c650?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2094&q=80',
+    'https://images.unsplash.com/photo-1534161308652-fdfcf10f62c4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2174&q=80'
+  ];
+
+  const images = slides.map(slide => {
+    return (
+      <SwiperSlide tag="li">
+        <img
+          src={`${slide}`}
+          style={{ height: '100vh', width: '100%' }}
+          alt=""
+        />
+      </SwiperSlide>
+    );
+  });
+
+  return (
+    <>
+      <Swiper
+        tag="section"
+        wrapperTag="ul"
+        id="main"
+        onSwiper={swiper => console.log(swiper)}
+        onSlideChange={() => console.log('slide change')}>
+        {images}
+      </Swiper>
+    </>
+  );
+}
+
+export default App;
