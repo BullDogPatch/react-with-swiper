1f0001c57deffca4e6554f6961a2e288ef98eab6
Craig Clayton <craig__clayton@outlook.com>
2020-08-08 23:28:00 +0100

    moved images to public

--- src/App.js
+++ src/App.js
@@ -1,44 +1,48 @@
 import React from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import SwiperCore from 'swiper';
+import SwiperCore, { Navigation, Pagination } from 'swiper';
 import 'swiper/swiper-bundle.css';
-
 import './styles.css';
+import Header from './components/Header';
+
+SwiperCore.use([Navigation, Pagination]);
 
 function App() {
   const slides = [
-    // './assets/patchSwiperImage2.jpg',
-    'https://images.unsplash.com/photo-1449034446853-66c86144b0ad?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2100&q=80',
-    'https://images.unsplash.com/photo-1470341223622-1019832be824?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2288&q=80',
-    'https://images.unsplash.com/photo-1448630360428-65456885c650?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2094&q=80',
-    'https://images.unsplash.com/photo-1534161308652-fdfcf10f62c4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2174&q=80'
+    './images/patchSwiperImage.jpg',
+    './images/patchSwiperImage1.jpg',
+    './images/patchSwiperImage2.jpg',
+    './images/patchSwiperImage3.jpg',
+    './images/patchSwiperImage4.jpg',
+    './images/patchSwiperImage5.jpg'
   ];
 
   const images = slides.map(slide => {
     return (
-      <SwiperSlide tag="li">
-        <img
-          src={`${slide}`}
-          style={{ height: '100vh', width: '100%' }}
-          alt=""
-        />
-      </SwiperSlide>
+      <>
+        <SwiperSlide tag="li">
+          <img
+            src={`${slide}`}
+            style={{ height: '60vh', width: '100%' }}
+            alt=""
+          />
+        </SwiperSlide>
+      </>
     );
   });
 
   return (
     <>
+      <Header />
       <Swiper
         tag="section"
         wrapperTag="ul"
         id="main"
+        navigation
+        pagination
         onSwiper={swiper => console.log(swiper)}
         onSlideChange={() => console.log('slide change')}>
         {images}
-        <SwiperSlide>This is hard to figure out</SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
       </Swiper>
     </>
   );
