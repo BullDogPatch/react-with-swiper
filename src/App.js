496f9637bce8b2b3441a2203457acea20a1afd07
Craig Clayton <craig__clayton@outlook.com>
2020-08-08 22:49:46 +0100

    .

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import './styles.css';
 
 function App() {
   const slides = [
-    './assets/patchSwiperImage.jpg',
+    // './assets/patchSwiperImage2.jpg',
     'https://images.unsplash.com/photo-1449034446853-66c86144b0ad?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2100&q=80',
     'https://images.unsplash.com/photo-1470341223622-1019832be824?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2288&q=80',
     'https://images.unsplash.com/photo-1448630360428-65456885c650?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2094&q=80',
@@ -35,6 +35,10 @@ function App() {
         onSwiper={swiper => console.log(swiper)}
         onSlideChange={() => console.log('slide change')}>
         {images}
+        <SwiperSlide>This is hard to figure out</SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>
       </Swiper>
     </>
   );
